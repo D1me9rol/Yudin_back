6774f2f4772661db262dd04aa648d706690fef45
D1me9rol <joudindmitriy1@gmail.com>
2024-12-24 18:25:06 +0300

    Добавьте файлы проекта.

---
+++ Models/Book.cs
@@ -0,0 +1,30 @@
+﻿namespace Yudin_back.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+        public string Genre { get; set; }
+        public bool IsAvailable { get; set; }
+
+        public List<Borrowing> Borrowings { get; set; } = new();
+
+        public string GetDescription()
+        {
+            return $"{Title} ({Author}, {Year})";
+        }
+
+        public void MarkAsUnavailable()
+        {
+            IsAvailable = false;
+        }
+
+        public void MarkAsAvailable()
+        {
+            IsAvailable = true;
+        }
+
+    }
+}
---
+++ Models/Borrowing.cs
@@ -0,0 +1,42 @@
+﻿namespace Yudin_back.Models
+{
+    public class Borrowing
+    {
+        public int Id { get; set; }
+        public int MemberId { get; set; }
+        public int BookId { get; set; }
+        public DateTime BorrowDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+
+        public Member Member { get; set; }
+        public Book Book { get; set; }
+
+        public void MarkAsReturned()
+        {
+            ReturnDate = DateTime.Now;
+        }
+
+        public bool IsOverdue()
+        {
+            return (DateTime.Now - BorrowDate).Days > 30 && ReturnDate == null;
+        }
+
+        public int GetBorrowDuration()
+        {
+            return ReturnDate == null
+                ? (DateTime.Now - BorrowDate).Days
+                : (ReturnDate.Value - BorrowDate).Days;
+        }
+
+        public decimal CalculateFine()
+        {
+            if (!IsOverdue())
+            {
+                return 0;
+            }
+            int overdueDays = (DateTime.Now - BorrowDate).Days - 30;
+            return overdueDays * 5; // Допустим, штраф — 5 единиц за день
+        }
+
+    }
+}
